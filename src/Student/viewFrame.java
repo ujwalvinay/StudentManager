f815041291c80a63f554445aaf9fdaaf67022437
Ujwal <Walkerdoodle7@gmail.com>
2022-01-07 08:13:36 -0800

    ViewFrame added

---
+++ src/Student/viewFrame.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class viewFrame {
+	JFrame f;
+	JTable j;
+}
+
+
+
+//Packages to import
+
+public class JTableExamples {
+ // frame
+ JFrame f;
+ // Table
+ JTable j;
+
+}
+ 
