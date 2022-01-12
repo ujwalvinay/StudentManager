a86f3fd520cc3b6dfa06b6f2032975f37360a57b
Ujwal <Walkerdoodle7@gmail.com>
2022-01-12 05:46:26 -0800

    ViewFrame updated

--- src/Student/viewFrame.java
+++ src/Student/viewFrame.java
@@ -10,10 +10,14 @@ class viewFrame extends JFrame
 	JLabel Stdreg = new JLabel("Reg no. : ");
 	JLabel Stdmrk = new JLabel("Mark : ");
 	JLabel Stdgrde = new JLabel("Grade : ");
+	JLabel nameShow = new JLabel();
+	JLabel regnoShow = new JLabel();
+	JLabel markShow = new JLabel();
+	/*
 	JTextField nameShow = new JTextField();
 	JTextField regnoShow = new JTextField();
 	JTextField markShow = new JTextField();
-	
+	*/
 	viewFrame()
 	{
 		setTitle("View Data");
@@ -31,10 +35,15 @@ class viewFrame extends JFrame
 	}
 	public void setLocation()
 	{
+		
 		StdName.setBounds(20, 110, 200, 20);
         Stdreg.setBounds(20, 140, 200, 20);
         Stdmrk.setBounds(20, 170, 200, 20);
         Stdgrde.setBounds(20, 200, 200, 20);
+        nameShow.setBounds(80,110,200,20);
+        regnoShow.setBounds(80,140,200,20);
+        markShow.setBounds(80,170,200,20);
+        
 	}
 	public void addComponents()
 	{
@@ -46,6 +55,12 @@ class viewFrame extends JFrame
 		container.add(regnoShow);
 		container.add(markShow);
 		
+	}
+	void data(String name,String reg,String Mark)
+	{
+		nameShow.setText(name);
+		regnoShow.setText(reg);
+		markShow.setText(Mark);
 	}
 	
 }
